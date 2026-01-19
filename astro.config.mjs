f01519fcb0349d26f321933411c1cefb1716f5be
Edu Merida <edumeridamerida215@gmail.com>
2026-01-19 19:10:32 +0100

    Configure Astro Netlify adapter (hybrid SSR)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 import path from 'node:path';
 
 export default defineConfig({
   site: 'http://localhost:4321',
-  output: 'server',
+  output: 'hybrid',
   trailingSlash: 'ignore',
+
+  adapter: netlify(),
 
   integrations: [
     tailwind(),
