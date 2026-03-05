d4a23a4dba17e22e1bfd52b25a99c005abfc451f
Edu Merida <edumeridamerida215@gmail.com>
2026-03-05 01:16:50 +0100

    add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 import path from 'node:path';
 
 export default defineConfig({
   site: 'https://belgotours.com',
-  output: 'static',          // ✅ antes era hybrid (ya no existe)
+
+  output: 'static',
   trailingSlash: 'ignore',
 
-  adapter: netlify(),        // ✅ obligatorio para prerender=false
+  adapter: netlify(),
 
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap()
+  ],
 
   vite: {
     resolve: {
